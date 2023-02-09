adcd5255b286b63810b7502a5cd89cf0ee7bdf7e
mp-martin <martin.paper@gmail.com>
2023-02-09 09:40:28 +0100

    Map and markers are finished.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,37 +1,39 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {SearchContext} from "../../contexts/search.context";
-
+import {SimpleAdEntity} from "types";
 import './Map.css'
 import 'leaflet/dist/leaflet.css';
 import '../../utils/fix-map-icon'
+import {SingleAd} from "./SingleAd";
 
 
 export const Map = () => {
     const {search} = useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
 
     useEffect(() => {
-        console.log('Make request to search for', search)
+        (async () => {
+            const res = await fetch(`http://localhost:3001/ad/search/${search}`);
+            const ads = await res.json();
+            setAds(ads);
+        })();
     }, [search])
 
     return (<>
-        <h1>Search for: {search}</h1>
         <MapContainer center={[52.2257495, 21.0071009]} zoom={13}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; <a href='https://openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
             />
-            <Marker position={[52.2257495, 21.0071009]}>
-                <Popup>
-                    <h2>Warszawa</h2>
-                    <p>Stolica Polski</p>
-                </Popup>
-            </Marker>      <Marker position={[52.23, 21.01]}>
-                <Popup>
-                    <h2>Warszawa 2</h2>
-                    <p>Stolica Polski</p>
-                </Popup>
-            </Marker>
+            {ads.map(ad => {
+                return (
+                    <Marker position={[ad.lat, ad.lon]} key={ad.id}>
+                        <Popup>
+                            <SingleAd id={ad.id} />
+                        </Popup>
+                    </Marker>)
+            })}
         </MapContainer>
     </>)
 }
